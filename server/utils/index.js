63913f33309faeada552adfc4a23ebadee62eb6f
ahas11 <93216221+ahas11@users.noreply.github.com>
2024-06-04 13:07:57 -0400

    function change

--- server/utils/index.js
+++ server/utils/index.js
@@ -15,10 +15,11 @@ export const createJWT = (res, userID) => {
   const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
+  console.log("JWT Token:", token); // Add this log
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict", // Prevent CSRF attack
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+    sameSite: "strict",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
   });
 };
