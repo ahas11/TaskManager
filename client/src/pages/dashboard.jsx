423bde2ab7cf24341e45aeebb271430d8ad95a15
ahas11 <93216221+ahas11@users.noreply.github.com>
2024-06-17 18:29:45 -0400

    bug testing

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -103,6 +103,7 @@ const UserTable = ({ users }) => {
       <td>
         <p className={clsx("w-fit px-3 py-1 rounded-full text-sm", user?.isActive ? "bg-blue-200" : "bg-yellow-100")}>
           {user?.isActive ? "Active" : "Disabled"}
+          {console.log(user.isActive)}
         </p>
       </td>
       <td className='py-2 text-sm'>{moment(user?.createdAt).fromNow()}</td>
