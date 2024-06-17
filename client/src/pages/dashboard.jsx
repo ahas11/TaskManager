12fe3106c5c8736212213d67e5fefaa92bb47a49
ahas11 <93216221+ahas11@users.noreply.github.com>
2024-06-17 16:52:12 -0400

    bug testing

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -142,6 +142,7 @@ const dashboard = () => {
 
   const totals = data?.tasks || {}; // Ensure totals is an object
 
+  console.log("This is the totals", totals)
   const stats = [
     {
       _id: "1",
@@ -173,13 +174,14 @@ const dashboard = () => {
     },
   ];
 
+  console.log("This is the totals", stats)
+
   const Card = ({ label, count, bg, icon }) => {
     return (
       <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
         <div className='h-full flex flex-1 flex-col justify-between'>
           <p className='text-base text-gray-600'>{label}</p>
           <span className='text-2xl font-semibold'>{count}</span>
-          <span className='text-sm text-gray-400'>{"110 last month"}</span>
         </div>
         <div className={clsx("w-10 h-10 rounded-full flex items-center justify-center text-white", bg)}>
           {icon}
