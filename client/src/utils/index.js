37d6e4f4b49fc83222087c020dce3cd3cd42c284
ahas11 <93216221+ahas11@users.noreply.github.com>
2024-10-21 22:03:23 -0400

    Bug testing

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -27,7 +27,7 @@ export function dateFormatter(dateString) {
 export function getInitials(fullName) {
     const names = fullName.split(" ");
 
-    const initials = names.slice(0, 2).map((name) => name[0].toUpperCase());
+    const initials = names.slice(0, 2).map((name) => name[0]);
 
     const initialsStr = initials.join("");
 
